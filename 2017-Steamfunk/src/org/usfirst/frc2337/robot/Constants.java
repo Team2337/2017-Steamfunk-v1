c5385edfd992f5a53e1fd549e0c28b6e06d546ec
Rhys Barrie <rhys.barrie@gmail.com>
2017-01-15 11:56:45 -0500

    Added Global Constants
    New (empty) class called Constants. Used for overall variables such as joystick sensitivity, maybe PID values, etc.

---
+++ 2017-Steamfunk/src/org/usfirst/frc2337/robot/Constants.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc2337.robot;
+
+/**
+ * Used as a global variable repository for variables such as
+ * the drive sensitivity, (maybe) PID values, or other such overall
+ * constants.
+ */
+public class Constants {
+	// Insert global variables in the following format:
+	// public final double kSUBSYSTEM_VARIABLE = VALUE;
+	
+	
+}
