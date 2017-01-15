c285a932ea0ebe8386fc16fa4c2db5a7939ae4e9
Rhys Barrie <rhys.barrie@gmail.com>
2017-01-15 10:07:52 -0500

    Fixed FuelShooter stop/start
    Wouldn't stop b/c default command was to resume/update the FuelShooter's speed, overriding the stop command. Now only updates when instructed, i.e. when the currentSpeed is changed.

--- 2017-Steamfunk/src/org/usfirst/frc2337/robot/subsystems/FuelShooter.java
+++ 2017-Steamfunk/src/org/usfirst/frc2337/robot/subsystems/FuelShooter.java
@@ -30,7 +30,6 @@ public class FuelShooter extends Subsystem {
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
-		setDefaultCommand(new FuelShooter_resumeShooter());
 		
 	}
 	
